8df71c227c046e95be67e9da207dfc6bd76f9bb8
Jason Brady <evervision@hotmail.com>
2015-11-07 10:40:58 -0800

    Missing code from episode #16

--- 17 - Three Way Data Binding/scripts/main.js
+++ 17 - Three Way Data Binding/scripts/main.js
@@ -29,7 +29,19 @@ var App = React.createClass({
       context : this,
       state : 'fishes'
     });
-    
+
+    var localStorageRef = localStorage.getItem('order-' + this.props.params.storeId);
+
+    if(localStorageRef) {
+      // update our component state to reflect what is in localStorage
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
+
+  },
+  componentWillUpdate : function(nextProps, nextState) {
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
   },
   addToOrder : function(key) {
     this.state.order[key] = this.state.order[key] + 1 || 1;
@@ -168,7 +180,7 @@ var Order = React.createClass({
     }
 
     return (
-      <li>
+      <li key={key}>
         {count}lbs
         {fish.name}
         <span className="price">{h.formatPrice(count * fish.price)}</span>

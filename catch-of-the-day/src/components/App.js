1585c9492ec36fd1fdb9d57b0ae3078372fcc80e
liz-substantial <liz@substantial.com>
2016-12-16 13:41:54 -0800

    more more

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -9,9 +9,9 @@ class App extends React.Component {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
-					<Header />
-					
+					<Header tagline="Fresh Seafood Market"/>
 				</div>
+				
 				<Order />
 				<Inventory />
 			</div>
--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 class Header extends React.Component {
 	render(){
+		console.log(this);
 		return(
-			<p>Header</p>	
+			<header className="top">
+				<h1>Catch 
+					<span className="ofThe">
+						<span className="of">of</span>
+						<span className="the">the</span>
+					</span>
+				Day</h1>
+				<h3 className="tagline"><span>{this.props.tagline}</span></h3>
+			</header>	
 		)
 	}
 }

e44b0725f14136450f0e38c1485aec280e0af28c
liz-substantial <liz@substantial.com>
2016-12-16 12:54:51 -0800

    more changes

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,13 +3,14 @@ import React from 'react';
 // use Capitals to mark what is a reusable component
 class StorePicker extends React.Component {
 	render() {
+		{/* This is a comment in JSX o_o. Between the return and the first AND ONLY allowed element is NOT okay. */}
 		return (
-			<div>
-				<form className="store-selector">Hiya
-				</form>
-				<h1>Hello World</h1>
-				<p>This is a small thing</p>
-			</div>
+			<form className="store-selector"> 
+				{/* This is also an acceptable comment placement in JSX o_o */}
+				<h2>Please enter a Store</h2>
+				<input type="text" required placeholder="Store Name" />
+				<button type="submit"> Visit Store! </button>
+			</form>
 		)
 	}
 }
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -3,7 +3,7 @@
 // this first line required in every file
 import React from 'react';
 import { render } from 'react-dom';
-
+import './css/style.css';
 
 // import components
 import StorePicker from './components/StorePicker';

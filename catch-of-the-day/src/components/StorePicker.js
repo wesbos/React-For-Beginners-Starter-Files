f889ae2f29b3674eef625644544e963fd2052ede
liz-substantial <liz@substantial.com>
2016-12-16 12:44:07 -0800

    starting to mess with jsx

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// use Capitals to mark what is a reusable component
+class StorePicker extends React.Component {
+	render() {
+		return (
+			<div>
+				<form className="store-selector">Hiya
+				</form>
+				<h1>Hello World</h1>
+				<p>This is a small thing</p>
+			</div>
+		)
+	}
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,11 @@
-// let's go!
+// let's go! 
+
+// this first line required in every file
+import React from 'react';
+import { render } from 'react-dom';
+
+
+// import components
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));

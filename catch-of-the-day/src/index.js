4791c48693201a8572a45852c3d3c0cf9d378494
Lisa Snider <snider_lisa@bah.com>
2016-12-31 17:03:01 -0500

    react components, jsx, css, layout, dynamic data w props, stateless functional components, react router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,27 @@
-// let's go!
+import React from 'react';
+import { BrowserRouter, Match, Miss } from 'react-router';
+import App from './components/App'
+import { render } from 'react-dom';
+import './css/style.css';
+
+
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern='/' component={StorePicker}/>
+                <Match exactly pattern='/store/:storeId' component={App}/>
+                <Miss component={NotFound}/>
+            </div>
+        </BrowserRouter>
+
+    ) 
+}
+// render (component, where in HTML it should be put)
+render(<Root/>, document.querySelector('#main'));
+
+// Routing if I;m on a store page, show me app component
+

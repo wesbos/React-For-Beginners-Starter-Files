a7f145ca822d6c35ee44be264545f752d23ab1a8
Ben Schinn <schinn.ben@gmail.com>
2017-01-17 17:28:21 -0700

    updating state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -13,6 +13,8 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+    
     //getinitialstate
     this.state = {
       fishes: {},
@@ -36,6 +38,16 @@ class App extends React.Component {
       fishes: sampleFishes
     })
   }
+
+  addToOrder(key) {
+    //take a copy of our state
+    const order = {...this.state.order};
+    //update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    //update our state
+    this.setState({ order: order})
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -45,7 +57,7 @@ class App extends React.Component {
           {
             Object
               .keys(this.state.fishes)
-              .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
           }
         </ul>
         </div>
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,7 +3,9 @@ import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
   render() {
-    const details = this.props.details;
+    const { details, index } = this.props;
+    const isAvailable = details.status === 'available';
+    const buttonText = isAvailable ? 'Add to Order' : 'Sold Out!'
     return(
       <li className="menu-fish">
         <img src={details.image} alt={details.name}/>
@@ -12,7 +14,7 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
-        <button>Add to Order</button>
+        <button onClick={() => this.props.addToOrder(index)} disabled={!isAvailable}>{buttonText}</button>
       </li>
     )
   }

b02d1e455088f46901cbba8d9b8ce6adf2c37e3f
Ben Schinn <schinn.ben@gmail.com>
2017-01-18 12:37:44 -0700

    small change in html

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -22,7 +22,7 @@ class StorePicker extends React.Component {
       <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter a Store</h2>
       <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
-    <button type="submit">Visit Store</button>
+    <button type="submit">Visit Store!</button>
       </form>
     )
   }

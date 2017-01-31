460f5ff3c7e585195230b14faa1320437cc866ce
Jennie S Yip <jennie.yip@udacity.com>
2017-01-31 14:39:33 -0800

    add(react-app): StorePicker init

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,11 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom'; // only import 'render' method from ReactDOM (vs importing entire package)
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>Hello</p>
+  }
+}
+
+render(<StorePicker/>, document.querySelector('#main'));

4c2b2a971ddf8e6ee449ab79abe8a2c478efed14
Michael Friedman <contact@michaeldavidfriedman.com>
2017-02-10 12:47:36 -0800

    Seperated first component, StorePicker into components folder and imported it into index.js

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class StorePicker extends Component {
+  render () {
+    return (
+      <div>
+        JSX Here
+      </div>
+    )
+  }
+}
+
+export default StorePicker
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,5 @@
-// let's go!
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import StorePicker from './components/StorePicker'
+
+render(<StorePicker />, document.getElementById('root'))

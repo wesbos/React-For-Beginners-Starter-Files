a467ced652714cd3c2baa51caf44b93e2ba15fc2
Michael Friedman <contact@michaeldavidfriedman.com>
2017-02-10 13:02:25 -0800

    Basic Store Picker Form setup.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,7 +4,17 @@ class StorePicker extends Component {
   render () {
     return (
       <div>
-        JSX Here
+        <form className='store-selector'>
+          <h2>Please Enter A Store</h2>
+          <input
+            onChange={this.handleInputChange}
+            required Placeholder='Store Name'
+            type='text'
+          />
+          <button type='submit'>
+            Visit Store
+          </button>
+        </form>
       </div>
     )
   }
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import StorePicker from './components/StorePicker'
+import './css/style.css'
 
 render(<StorePicker />, document.getElementById('root'))

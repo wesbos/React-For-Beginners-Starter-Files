8ead55d29a7b683d7f3735d877032cb14634badd
yenpraja610 <yen.a@me.com>
2017-06-19 16:50:04 -0400

    Change Header.js from regular component to stateless Implement Browser Router in index.js with miss and match methods

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,22 +1,20 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    console.log(this);
-    return(
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">of</span>
-            <span className="the">the</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline">{this.props.test}</h3>
-      </header>
-    )
-  }
+const Header = (props) => {
+  return(
+    <header className="top">
+      <h1>
+        Catch
+        <span className="ofThe">
+          <span className="of">of</span>
+          <span className="the">the</span>
+        </span>
+        Day
+      </h1>
+      <h3 className="tagline">{props.tagline}</h3>
+    </header>
+  )
 }
+
 
 export default Header;

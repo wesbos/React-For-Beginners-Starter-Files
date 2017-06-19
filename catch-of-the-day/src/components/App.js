965fe6e7b49589bcb0ad6c4b03ec4df790ad2fd5
yenpraja610 <yen.a@me.com>
2017-06-19 13:45:50 -0400

    Implement components in App.js

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -16,3 +16,5 @@ class App extends React.Component {
     )
   }
 }
+
+export default App;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+
+render(<App/>, document.querySelector('#main'));

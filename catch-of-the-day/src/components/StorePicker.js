85ad37bbc5e3f66ed5eb1a89895422fcba131d6c
yenpraja610 <yen.a@me.com>
2017-06-19 13:38:25 -0400

    building env

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      //anywhere else outside of jsx syntax comments can be entered the normal
+      //way.Like here
+      <form className="store-selector">
+        {/*this is the format to enter comments in jsx*/}
+        <h2>Please enter a store</h2>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="Submit">Visit Store </button>
+      </form>
+    )
+  }
+
+}
+
+export default StorePicker;

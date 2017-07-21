4122928d92b04b397d08be09f514d375a64f0997
Stef <stefania.cola1@gmail.com>
2017-07-21 10:59:27 +1000

    Local changes from day 1-15

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AddFishForm from './AddFishForm';
+
+class Inventory extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish}/>
+        <button onClick={this.props.loadSamples}>Add Sample Fish</button>
+      </div>
+
+    )
+  }
+}
+
+export default Inventory;

5d26d75a1da8508f5998dc45fed96e231eddcb27
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-08-10 17:53:52 -0700

    component scaffold

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './css/style.css'
+import App from './components/App'
 
-import StorePicker from './components/StorePicker'
+import StorePicker from './components/StorePicker' //eslint-disable-line
 
-render(<StorePicker/>, document.querySelector('#main'))
+render(<App/>, document.querySelector('#main'))

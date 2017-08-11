fe010698b2af8b947ada5cd8d48f2da62d437b41
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-08-11 12:19:05 -0700

    adding to order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
-// import AddFishForm from './AddFishForm'
+import Fish from './Fish'
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
   constructor() {
     super() //can't use the keyword 'this' until super is called
     this.addFish = this.addFish.bind(this)
+    this.loadSamples = this.loadSamples.bind(this)
+    this.addToOrder = this.addToOrder.bind(this)
+
     this.state = {
       fishes: {},
       order: {}
@@ -21,14 +25,35 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  loadSamples() {
+
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
+
+  addToOrder(key) {
+    const order = {...this.state.order}
+    order[key] = order[key] + 1 || 1
+    this.setState({ order })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
         <Header tagline="Fresh Seafood Market" />
+        <ul className="list-of-fishes">
+          {
+            Object
+            .keys(this.state.fishes)
+            .map(key => <Fish key={key} index={key} details={this.state.fishes[key]}
+              addToOrder={this.addToOrder}/>)
+          }
+        </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }

fa4ba9dccd776c14d008ba3e7505aba975705606
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-08-10 18:10:50 -0700

    fun function functionality

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component{
   render() {
     return (
       <form className="store-selector">
         <h2>Please Enter A Store</h2>   { /* this is how you comment in JSX */}
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     )
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Match, Miss } from 'react-router'
 import './css/style.css'
+
 import App from './components/App'
-
 import StorePicker from './components/StorePicker' //eslint-disable-line
+import NotFound from './components/NotFound'
 
-render(<App/>, document.querySelector('#main'))
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'))

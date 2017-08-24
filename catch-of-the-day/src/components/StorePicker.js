1372c23ee4732e258bd4dc3c12e8bd477b82c827
Callan Morrison <callan.l.morrison@gmail.com>
2017-08-23 22:54:12 -0500

    add in styles

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,7 +4,7 @@ class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
-        { /* PS: This comment can't go above the form opening tag. Dang JSX */ }
+        { /* PS: This comment can't go above the form opening tag. Dang JSX. */ }
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder='Store Name'/>
         <button type="submit">Visit Store -></button>

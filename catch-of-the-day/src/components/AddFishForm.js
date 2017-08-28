fba773b10171a3ca3222eb6ae885235adeea8843
Callan Morrison <callan.l.morrison@gmail.com>
2017-08-27 23:19:32 -0500

    init create fish

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 class AddFishForm extends React.Component {
+  createFish(e) {
+    e.preventDefault();
+    console.log("Yay")
+  }
+
   render() {
     return (
-      <form className="fish-edit">
+      <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
         <input type="text" placeholder="Fish Name"/>
         <input type="text" placeholder="Fish Price"/>
         <select>

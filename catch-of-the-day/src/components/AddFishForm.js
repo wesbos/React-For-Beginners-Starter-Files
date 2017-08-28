b907a606aec1da8a8f1237ca7a39417595c1e102
Callan Morrison <callan.l.morrison@gmail.com>
2017-08-27 23:27:26 -0500

    retreiving fish values from form:

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -3,20 +3,27 @@ import React from 'react';
 class AddFishForm extends React.Component {
   createFish(e) {
     e.preventDefault();
-    console.log("Yay")
+    const fish = {
+      name: this.name.value,
+      prcie: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value
+    }
+    console.log(fish);
   }
 
   render() {
     return (
       <form className="fish-edit" onSubmit={(e) => this.createFish(e)}>
-        <input type="text" placeholder="Fish Name"/>
-        <input type="text" placeholder="Fish Price"/>
-        <select>
+        <input ref={(input) => this.name = input} type="text" placeholder="Fish Name"/>
+        <input ref={(input) => this.price = input} type="text" placeholder="Fish Price"/>
+        <select ref={(input) => this.status = input}>
           <option value="avaliable">Fresh!</option>
           <option value="unavaliable">Sold Out!</option>
         </select>
-        <textarea type="text" placeholder="Fish Desc"></textarea>
-        <input type="text" placeholder="Fish Image"/>
+        <textarea ref={(input) => this.desc = input} type="text" placeholder="Fish Desc"></textarea>
+        <input ref={(input) => this.image = input} type="text" placeholder="Fish Image"/>
         <button type="submit"></button>
       </form>
     )

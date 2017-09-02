9f2dfa385df7f50b12d79997528a7bca5158a2b4
Simone Holder <simone@simoneholder.com>
2017-09-02 17:40:41 -0400

    video5

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+	render() {
+		return(
+			<form className="store-selector">
+				<h2>Please enter a store</h2>
+				<input type="text" required placeholder="Store Name"/>
+				<button type="submit">Visit Store ↳</button>
+			</form>
+		)
+	}
+}
+
+export default StorePicker;

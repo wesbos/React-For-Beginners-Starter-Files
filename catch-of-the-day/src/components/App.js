a1169bfb8331f43b39414c6692759b857ec64a11
Callan Morrison <callan.l.morrison@gmail.com>
2017-09-13 23:01:28 -0500

    loading sample fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
           <ul className="list-of-fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish/>)}
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
           </ul>
         </div>
         <Order/>

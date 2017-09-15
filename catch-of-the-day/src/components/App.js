06335ebf0b125338114c71bab0cdfce2d1e36c0e
Callan Morrison <callan.l.morrison@gmail.com>
2017-09-15 00:09:35 -0500

    persisting state in local storage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -26,10 +26,22 @@ class App extends React.Component {
       context: this,
       state: 'fishes'
     })
+
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+    if (localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
+    }
   }
 
   componentWillUnmount() {
     base.removeBinding(this.ref);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order))
   }
 
   addFish(fish) {
@@ -57,10 +69,10 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market"/>
           <ul className="list-of-fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
+            {Object.keys(this.state.fishes).map(key => <Fish key={key}  index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Order params={this.props.params} fishes={this.state.fishes} order={this.state.order}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadFishes}/>
       </div>
     )

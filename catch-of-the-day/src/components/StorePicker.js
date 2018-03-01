af14aa8c12303260b80942970c04af00556bf67f
Greg Rickaby <gregrickaby@gmail.com>
2018-02-28 23:22:32 -0600

    first commit

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <Fragment>`
+            <p>Heyyyy</p>
+            <div>What up!?</div>
+            </Fragment>
+        )
+    };
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,5 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker />, document.querySelector('#main'));

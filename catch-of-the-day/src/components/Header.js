189098c1b7d843407979f322c5be47c2d9043dbd
Greg Rickaby <gregrickaby@gmail.com>
2018-03-01 00:23:43 -0600

    completed #8

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,26 +1,21 @@
 import React from "react";
-import { Fragment } from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        <header className="top">
-          <h1>
-            Catch
-            <span className="ofThe">
-              <span className="of">Of</span>
-              <span className="the">The</span>
-            </span>
-            Day
-          </h1>
-          <h3 className="tagline">
-            <span>Fresh Seafood Market</span>
-          </h3>
-        </header>
-      </Fragment>
-    );
-  }
-}
+// A "stateless functional component"
+// Just returns data after receiving props...
+const Header = props => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
 
 export default Header;

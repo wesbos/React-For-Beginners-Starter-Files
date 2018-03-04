cac0e5cf3eab5d577373710e6638f39f8550e84c
Greg Rickaby <gregrickaby@gmail.com>
2018-03-03 21:18:09 -0600

    hook in rout and push()

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,21 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  storeName = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.storeName.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input
           type="text"
+          ref={this.storeName}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}

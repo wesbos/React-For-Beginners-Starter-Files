8f3a7530b6606ebc9c6ce884e2afeb99bfcddbfd
Greg Rickaby <gregrickaby@gmail.com>
2018-03-06 20:06:09 -0600

    setting state and a working form 😎

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,17 +1,52 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+  // Set up Refs.
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  // Create fish function.
+  createFish = event => {
+    // Stop the form from submitting.
+    event.preventDefault();
+    // Set our inventory.
+    const fish = {
+      name: this.nameRef.value.value,
+      price: parseFloat(this.priceRef.value.value),
+      status: this.statusRef.value.value,
+      desc: this.descRef.value.value,
+      image: this.imageRef.value.value
+    };
+    // Update props.
+    this.props.addFish(fish);
+    // Refresh the form.
+    event.currentTarget.reset();
+  };
+
   render() {
     return (
-      <form className="fish-edit">
-        <input name="name" type="text" placeholder="Name" />
-        <input name="price" type="text" placeholder="Price" />
-        <select name="status">
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input
+          name="price"
+          ref={this.priceRef}
+          type="text"
+          placeholder="Price"
+        />
+        <select name="status" ref={this.statusRef}>
           <option value="available">Fresh</option>
           <option value="unavailable">Sold Out</option>
         </select>
-        <textarea name="desc" placeholder="Description" />
-        <input name="image" type="text" placeholder="Image" />
+        <textarea name="desc" ref={this.descRef} placeholder="Description" />
+        <input
+          name="image"
+          ref={this.imageRef}
+          type="text"
+          placeholder="Image"
+        />
         <button type="submit">+ Add Fish</button>
       </form>
     );
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,19 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  // Set initial state.
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // Make a copy of existing state.
+    const fishes = { ...this.state.fishes };
+    // Add new fish to fishes const.
+    fishes[`fish${Date.now()}`] = fish;
+    // Set new fishes object to state.
+    this.setState({ fishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +24,7 @@ class App extends React.Component {
           <Header tagline="greg is cool" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }

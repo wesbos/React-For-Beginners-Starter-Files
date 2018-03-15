01ccf194d57903f0227840336b0ad4db2d352ed9
Timothy Myers <tmyers@frontsteps.com>
2018-03-15 13:27:09 -0600

    added css and index.js to chapter 14 stepped solutions

---
+++ stepped-solutions/14/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import Router from "./components/Router";
+import "./css/style.css";
+
+render(<Router />, document.querySelector("#main"));

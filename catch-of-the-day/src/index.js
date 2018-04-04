2d646c3dd0efbc8ddd9ce82a74499d72962b5527
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-04-04 14:55:26 -0300

    Loading CSS into our React Application

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
 import StorePicker from './components/StorePicker';
 
 render(<StorePicker />, document.querySelector('#main'));

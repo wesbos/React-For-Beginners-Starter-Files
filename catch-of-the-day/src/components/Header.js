4aaeacf2e53dd6dfb6148830bd9c0d0c5da95c3d
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-04-05 11:57:25 -0300

    Stateless Func. Components
    Video number 7-8

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">Of</span>
-            <span className="the">The</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline">
-          <span>Fresh Daily</span>
-        </h3>
-      </header>
-    );
-  }
-}
+const Header = props => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+
+Header.propTypes = {
+  tagline: PropTypes.string,
+};
+
+Header.defaultProps = {
+  tagline: '',
+};
 
 export default Header;

7a1452a2e7f14061cb76b12706a350d68a1167a7
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-04-05 18:02:27 -0300

    Handling events
    Used constructor to bind this to method, arrow function in class property didn't work - will test to see why

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  constructor() {
+    super();
+
+    this.goToStore = this.goToStore.bind(this);
+  }
+
+  myInput = React.createRef();
+
+  // goToStore = event => {
+  //   event.preventDefault();
+
+  //   const storeName = this.myInput.value.value;
+  //   this.props.history.push(`/store/${storeName}`);
+  // };
+
+  goToStore(event) {
+    event.preventDefault();
+
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     );
   }
 }
+
+StorePicker.propTypes = {
+  history: PropTypes.object.isRequired,
+};
 
 export default StorePicker;

f6be4f4372447b0ea436b4734f01ca8553c5bc26
Jesse Mezini <jesse@Mac-mini-de-Jesse.local>
2018-04-06 16:45:29 -0300

    Changed order - inventory order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,8 +10,8 @@ class App extends Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory />
       </div>
     );
   }

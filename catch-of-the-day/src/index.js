e5be57e2304ecef5d0dc4ef13e8b4906627565b0
scottyb <scottyb@scottybs-MacBook.local>
2018-05-07 14:36:00 -0700

    store picker component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert('hey');
+import React from 'react';
+import {render} from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+render(<StorePicker />, document.getElementById('main'))

29871d60c413903d55dff8b7630a03d9ea5534cf
scottyb <scottyb@scottybs-MacBook.local>
2018-05-13 15:33:26 -0700

    importing css

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>I am the store Picker!</p>
+        return (
+            <React.Fragment>
+            <form className = "store-selector">
+            <h2> Please Enter a Store </h2>
+            <input type ="text" required placeholder="Store Name" />
+            <button type="submit">Visit Store </button>
+            </form>
+            </React.Fragment>
+        )
     }
 }
 export default StorePicker;

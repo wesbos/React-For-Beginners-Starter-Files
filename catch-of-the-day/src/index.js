c98ed9b6a269babce9aa0fbb21e263f427ae77db
Ryan Kim <Ryan@Ryans-MBP.lan>
2018-07-18 22:05:15 -0700

    initial commit, prettier and eslint working

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>Hello!</p>;
+  }
+}
+
+render(<StorePicker />, document.querySelector('#main'));

e8332974ce9a4c6dc434e866aaaff16b395406fe
mahmutkaya <bajari88@hotmail.com>
2018-07-22 00:56:47 +0200

    deploying

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { render } from "react-dom";
-// import { StorePicker } from "./components/StorePicker";
-// import App from "./components/App";
 import "./css/style.css";
 import Router from "./components/Router";
 

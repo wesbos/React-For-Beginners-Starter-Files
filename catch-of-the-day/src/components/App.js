5f6f9ceb9ab9c729660418b1840a1bab557e06f1
jtawes <[jtawes510@gmail.com]>
2018-07-27 22:04:37 -0400

    up-to-lesson-16
    with all changes up to lesson 16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
 	state = {
@@ -20,14 +22,39 @@ class App extends React.Component {
 		});
 	};
 
+	loadSampleFishes = () => {
+		this.setState({ fishes: sampleFishes });
+	};
+
+	addToOrder = (key) => {
+		// 1. take a copy of state
+		const order = { ...this.state.order };
+		// 2. Either add to the order, or update the number in our order
+		order[key] = order[key] + 1 || 1;
+		// 3. Call setState to update our state object
+		this.setState({ order: order });
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market"/>
+					<ul className="fishes">
+						{Object.keys(this.state.fishes).map(key => 
+							<Fish 
+								key={key} 
+								index={key}
+								details={this.state.fishes[key]} 
+								addToOrder={this.addToOrder} 
+							/>)}
+					</ul>
 				</div>
 				<Order />
-				<Inventory addFish={this.addFish} />
+				<Inventory 
+					addFish={this.addFish} 
+					loadSampleFishes={this.loadSampleFishes} 
+				/>
 			</div>
 		)
 	}
---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { formatPrice } from '../helpers';
+
+class Fish extends React.Component {
+	handleClick = () => {
+		this.props.addToOrder(this.props.index);
+	}
+	render() {
+		const { image, name, price, desc, status } = this.props.details;
+
+		const isAvailable = status === 'available';
+
+		return (
+			<li className="menu-fish">
+				<img src={image} alt={name} />
+				<h3 className="fish-name">{name}
+					<span className="price">{formatPrice(price)}</span>
+				</h3>
+				<p>{desc}</p>
+				<button 
+					disabled={!isAvailable}
+					onClick={this.handleClick} >
+						{isAvailable ? 'Add to Order' : 'SOLD OUT!'}
+				</button>
+			</li>			
+		);
+	}
+};
+
+export default Fish;

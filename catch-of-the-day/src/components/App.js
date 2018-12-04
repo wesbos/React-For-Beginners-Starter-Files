19e701088595c46f162873bddc7d3e26e733c1b5
Sean Lawrence <slawrence@shoprunner.com>
2018-12-04 14:37:25 -0500

    displaying order state with jsx

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -47,7 +47,7 @@ class App extends React.Component {
 						)}
 					</ul>
 				</div>
-			<Order />
+			<Order fishes={this.state.fishes} order={this.state.order}/>
 			<Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
 			</div>
 		);

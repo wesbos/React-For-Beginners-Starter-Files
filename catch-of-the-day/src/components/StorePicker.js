cd44f4c6c07b9acf14eecaa5810b69cd7cea67e8
Sean Lawrence <slawrence@shoprunner.com>
2018-12-03 19:50:36 -0500

    added event handling

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -7,8 +7,9 @@ class StorePicker extends React.Component {
 		// 1. stop the form from submitting
 		event.preventDefault();
 		// 2. get the text from that input
-
+		const storeName = this.myInput.value.value;
 		// 3. change the page to /store/whatever-they-entered
+		this.props.history.push(`/store/${storeName}`)
 	}
 	render() {
 		return (

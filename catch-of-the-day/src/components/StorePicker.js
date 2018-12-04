084a35cd96305ba47ac17fd197203ff709f0d49a
Sean Lawrence <slawrence@shoprunner.com>
2018-12-03 19:39:52 -0500

    added property retrieve store name from input field

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+	goToStore = event => {
+		// 1. stop the form from submitting
+		event.preventDefault();
+		// 2. get the text from that input
+
+		// 3. change the page to /store/whatever-they-entered
+	}
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 			{/*This is a store picker!!*/}
 				<h2>Please Enter a Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+				<input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/>
 				<button type="submit">Visit Store -></button>
 			</form>
 		)		

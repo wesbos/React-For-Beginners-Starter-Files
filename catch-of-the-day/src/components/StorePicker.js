e70c038e76f3f4f9e7f7fd7623fc8bf0d94de4eb
Seth Schwartz <sschwartz214@gmail.com>
2018-12-10 01:24:31 -0700

    Adds form to StorePick comp

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class StorePicker extends React.Component {
   render() {
-    return <p>I am the sotre Picker!!</p>;
+    return (
+      <form action className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store -></button>
+      </form>
+    );
   }
 }
 
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import StorePicker from "./components/StorePicker";
+import "./css/style.css";
 
 render(<StorePicker />, document.querySelector("#main"));

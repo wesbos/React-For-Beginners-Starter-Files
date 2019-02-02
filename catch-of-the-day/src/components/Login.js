cb25963b753c0c2a4e2334a256678207746afff7
Jonathan Dahan <hi@jonathan.is>
2019-02-02 12:39:34 -0500

    Create useAuth hook for logging in
    This may not be the most idiomatic React or idiomatic usage of hooks,
    but the conversion seems to be working. Some things to look at:
    
    To auth on mount, an empty array is passed into useEffect in useAuth.
    
    The Login component now takes children, which feels more consistent
    conceptually, but maybe can be simplified with more liberal use of hooks.
    
    useAuth returns [{uid, owner}, {authenticate, logout}]. It might
    be best to create another higher level hook that returns something
    like { isOwner, isLoggedIn }.

--- catch-of-the-day/src/components/Login.js
+++ catch-of-the-day/src/components/Login.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useAuth } from '../hooks/useAuth.js'
 
-const Login = props => (
-  <nav className='login'>
-    <h2>Inventory Login</h2>
-    <p> sign in to manage your store's inventory </p>
-    <button className='github' onClick={() => props.authenticate('Github')}>
-      Login with Github
-    </button>
-  </nav>
-)
+const Login = props => {
+  const { storeId, children } = props
+  const [{ uid, owner }, { authenticate, logout }] = useAuth(storeId)
+
+  if (!uid) {
+    return (
+      <nav className='login'>
+        <h2>Inventory Login</h2>
+        <p> sign in to manage your store inventory </p>
+        <button className='github' onClick={() => authenticate('Github')}>
+          Login with Github
+        </button>
+      </nav>
+    )
+  }
+
+  if (uid !== owner) {
+    return (
+      <div>
+        <h2>You have no power here!</h2>
+        <button onClick={logout}>Log Out!</button>
+      </div>
+    )
+  }
+
+  return children
+}
 
 Login.propTypes = {
-  authenticate: PropTypes.func.isRequired
+  storeId: PropTypes.string.isRequired
 }
 export { Login }
---
+++ catch-of-the-day/src/hooks/useAuth.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase'
+import base, { firebaseApp } from '../base.js'
+import { useState, useEffect } from 'react'
+
+/// useAuth takes a storeId, and returns the uid and owner of that store
+const useAuth = storeId => {
+  const [uid, setUid] = useState(null)
+  const [owner, setOwner] = useState(null)
+
+  // We have authenticated
+  const authHandler = async auth => {
+    if (!auth) return
+    const { uid } = auth.user
+
+    const store = await base.fetch(storeId, { context: this }) // TODO ????
+
+    if (!store.owner) await base.post(`${storeId}/owner`, { data: uid })
+
+    setUid(uid)
+    setOwner(store.owner || uid)
+  }
+
+  // Try to auth on first mount silently, in case of refresh
+  useEffect(() => firebase.auth().onAuthStateChanged(authHandler), [])
+
+  const authenticate = provider => {
+    const authProvider = (() => {
+      switch (provider) {
+        case 'Github':
+          return new firebase.auth.GithubAuthProvider()
+        default:
+          return Error(`unknown auth provider '${provider}'`)
+      }
+    })()
+
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(authHandler)
+  }
+
+  const logout = async () => {
+    await firebase.auth().signOut()
+    setUid({ uid: null })
+  }
+
+  return [{ uid, owner }, { authenticate, logout }]
+}
+
+export { useAuth }

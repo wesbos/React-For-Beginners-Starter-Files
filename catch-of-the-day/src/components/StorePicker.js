efec6f477f31b6878bcd6ec7c2d37a1c63859522
Jonathan Dahan <hi@jonathan.is>
2019-02-02 13:50:46 -0500

    make StoreInput a function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,42 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
 import { getFunName } from '../helpers.js'
 
-class StorePicker extends React.Component {
-  storeInput = React.createRef()
+const StorePicker = props => {
+  const storeInput = useRef()
 
-  goToStore = event => {
+  const goToStore = event => {
     const {
-      storeInput: {
-        current: { value: storeName }
-      },
-      props: { history }
-    } = this
+      current: { value: storeName }
+    } = storeInput
+    const { history } = props
     event.preventDefault()
     history.push(`/store/${storeName}`)
   }
 
-  render () {
-    return (
-      <>
-        {/* react needs a single element in render returns, so we wrap */}
-        {/* babel is converting <> to <React.Fragment>, I think */}
-        <h1> fishing time </h1>
-        <form className='store-selector' onSubmit={this.goToStore}>
-          <h2> please enter a store </h2>
-          <input
-            ref={this.storeInput}
-            type='text'
-            required
-            placeholder='store name'
-            defaultValue={getFunName()}
-          />
-          <button type='submit'>visit store ↣</button>
-        </form>
-      </>
-    )
-  }
+  return (
+    <>
+      {/* react needs a single element in render returns, so we wrap */}
+      {/* babel is converting <> to <React.Fragment>, I think */}
+      <h1> fishing time </h1>
+      <form className='store-selector' onSubmit={goToStore}>
+        <h2> please enter a store </h2>
+        <input
+          ref={storeInput}
+          type='text'
+          required
+          placeholder='store name'
+          defaultValue={getFunName()}
+        />
+        <button type='submit'>visit store ↣</button>
+      </form>
+    </>
+  )
 }
+
 StorePicker.propTypes = {
   history: PropTypes.object
 }

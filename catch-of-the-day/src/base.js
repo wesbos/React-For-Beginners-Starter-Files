24f063d2ddfe1a4adeaefd50f4bcb6aed192dd39
Jonathan Dahan <hi@jonathan.is>
2019-02-02 11:35:38 -0500

    rename firebaseapp

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,14 +1,14 @@
 import Rebase from 're-base'
 import { initializeApp } from 'firebase'
 
-const firebase = initializeApp({
-  apiKey: "AIzaSyBrv7gtEDGn8Jr90kCI-ZD-CDQMxUiMHU4",
-  authDomain: "catch-of-the-day-92a28.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-92a28.firebaseio.com"
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyBrv7gtEDGn8Jr90kCI-ZD-CDQMxUiMHU4',
+  authDomain: 'catch-of-the-day-92a28.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-92a28.firebaseio.com'
 })
 
-const base = Rebase.createClass(firebase.database())
+const base = Rebase.createClass(firebaseApp.database())
 
-export { firebase }
+export { firebaseApp }
 
 export default base

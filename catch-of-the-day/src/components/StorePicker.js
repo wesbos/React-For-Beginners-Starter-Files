8827e832e7474a0e33e2cffba616b0e903bedd1e
John Kinane <jkinane@tenetpartners.com>
2019-03-31 15:31:49 -0400

    End of step 4

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
 class StorePicker extends React.Component {
     render() {
-        return <p>Fuck You</p>
+       return (
+            <form className="store-selector">
+                <h2> Please Enter a Store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store</button>
+            </form>                        
+        )
     }
 }
 

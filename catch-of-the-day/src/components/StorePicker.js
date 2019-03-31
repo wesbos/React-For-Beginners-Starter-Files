b2f9ea4e290c9426d71fc889b47c0424db4c117f
John Kinane <jkinane@tenetpartners.com>
2019-03-31 15:03:26 -0400

    finished chap 3

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,8 @@
+import React from 'react';
+class StorePicker extends React.Component {
+    render() {
+        return <p>Fuck You</p>
+    }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,6 @@
-// let's go!
-alert("Hey fucker!");
+import React from "react";
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+render(<StorePicker />, document.querySelector('#main'));

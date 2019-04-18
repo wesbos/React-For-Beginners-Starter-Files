53aa1f73d18cbfe01a14089c88e0c810cabd2b8a
Kanika B <kanika.b@gmail.com>
2019-04-18 16:48:28 +0530

    Upto lesson 6

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+
+  render(){
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header></Header>
+        </div>
+        <Order></Order>
+        <Inventory></Inventory>
+      </div>
+    )
+  }
+}
+
+export default App;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import StorePicker from './components/StorePicker';
+import './css/style.css';
 
-render(<StorePicker></StorePicker>, document.querySelector("#main"));
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+
+render(<App></App>, document.querySelector("#main"));

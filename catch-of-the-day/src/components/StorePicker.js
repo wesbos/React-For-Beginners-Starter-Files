c70ace210250145d0b8014a8f1a98652894651dd
danvdev <46414048+danvdev@users.noreply.github.com>
2019-04-25 22:26:19 -0500

    React Fragment

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class StorePicker extends React.Component{
+	render(){
+		return (
+			<React.Fragment>
+			<p>You'll Be Hooked on Our Fresh Fish!</p>
+				<form className="store-selector">
+					<h2>Please Enter A Store</h2>
+				</form>
+			</React.Fragment>
+		)
+	}
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,7 @@
-// let's go!
-alert('hey');
+import React from 'react';
+import {render} from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+
+render(<StorePicker/>, document.querySelector('#main'));

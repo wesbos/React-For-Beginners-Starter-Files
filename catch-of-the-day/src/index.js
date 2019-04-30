74b659469d619f710b11b8c2066a4d15d4ac14f6
danvdev <46414048+danvdev@users.noreply.github.com>
2019-04-30 14:06:44 -0500

    scaffolding out site structure

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import {render} from 'react-dom';
-import StorePicker from './components/StorePicker';
-import './css/style.css';
+import React from "react";
+import {render} from "react-dom";
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+
+import "./css/style.css";
 
 
 
-render(<StorePicker/>, document.querySelector('#main'));
+//render(<StorePicker/>, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));

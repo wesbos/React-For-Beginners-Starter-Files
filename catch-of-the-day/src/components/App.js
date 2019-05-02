2c23c975d6d3b41976739570a17f9db13db35d5c
Ryan M <ryanmarion012@gmail.com>
2019-05-01 21:46:44 -0600

    completed the order functionality and display

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -46,7 +46,7 @@ export default class App extends React.Component{
             )}
           </ul>
         </div>
-        <Order/>
+        <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
 

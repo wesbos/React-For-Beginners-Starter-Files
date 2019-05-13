7c648811e9422710411f18751b9152cbe047035f
danvdev <46414048+danvdev@users.noreply.github.com>
2019-05-13 00:10:51 -0500

    adding new fish from form, passing add fish method through props, updating state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,22 @@ import Inventory from "./Inventory";
 
 
 class App extends React.Component{
+	state = {
+		fishes: {},
+		order:{}
+	};
+	addFish = (fish) => {
+		//always update state with these steps
+		//1. Take a copy/snapshot of existing state
+		const fishes = {...this.state.fishes};
+		//2. Add new instance to that variable
+		fishes[`fish${Date.now()}`] = fish;
+		//3. Set new fishes object to state
+		this.setState({
+			fishes:fishes
+		});
+
+	}
 	render(){
 		return(
 			<div className="catch-of-the-day">
@@ -12,7 +28,7 @@ class App extends React.Component{
 					<Header tagline="Fresh Seafood Market"/>
 				</div>
 				<Order/>
-				<Inventory/>
+				<Inventory addFish={this.addFish}/>
 			</div>
 		);
 	}

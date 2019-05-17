fe086c0977e38044d46e018e016d0c76726830f6
danvdev <46414048+danvdev@users.noreply.github.com>
2019-05-17 10:48:32 -0500

    adding fish to order in state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,8 @@ class App extends React.Component{
 		fishes: {},
 		order:{}
 	};
+
+
 	addFish = (fish) => {
 		//always update state with these steps
 		//1. Take a copy/snapshot of existing state
@@ -21,11 +23,21 @@ class App extends React.Component{
 		this.setState({
 			fishes:fishes
 		});
-
 	};
+
+
 	loadSampleFishes = () => {
 		this.setState({fishes: sampleFishes});
 	};
+
+	addToOrder = (key) => {
+		//1. take a copy of state
+		const order = {...this.state.order};
+		//2. either add to the order, or increment the number in our order
+		order[key] = order[key] + 1 || 1;
+		//3. call setstate to update our state object
+		this.setState ({order});
+	}
 
 	render(){
 		return(
@@ -33,7 +45,7 @@ class App extends React.Component{
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market"/>
 					<ul className="fishes">
-						{Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+						{Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}
 					</ul>
 				</div>
 				<Order/>

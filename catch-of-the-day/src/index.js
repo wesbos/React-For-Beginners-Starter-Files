c2bea8b3b4ce0840eebfafd1cd1bc4ce9aace923
Crystal Broach <crysland@gmail.com>
2019-12-30 19:10:21 -0600

    getting started with store picker, vid 4 complete

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker />, document.querySelector('#main'));

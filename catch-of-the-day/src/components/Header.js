c619760aecde19cb25163a33974408b7e0dbbf16
goborn <gtoborn@gmail.com>
2020-02-06 20:18:23 -0700

    Added Router component, modified Header component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,42 @@
 import React from 'react';
+// Standard way to create a component
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-                <h1>
-                    Catch
-                    <span className="ofThe">
-                        <span className="of">Of</span>
-                        <span className="the">The</span>
-                    </span>
-                    Day
-                </h1>
-                <h3 className="tagline">
-                    <span>{this.props.tagline}</span>
-                </h3>
-            </header>
-        );
-    }
-}
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className="top">
+//                 <h1>
+//                     Catch
+//                     <span className="ofThe">
+//                         <span className="of">Of</span>
+//                         <span className="the">The</span>
+//                     </span>
+//                     Day
+//                 </h1>
+//                 <h3 className="tagline">
+//                     <span>{this.props.tagline}</span>
+//                 </h3>
+//             </header>
+//         );
+//     }
+// }
+
+//new way to create a component that just passes props through it (Stateless)
+
+const Header = (props) => (
+    <header className="top">
+        <h1>
+            Catch
+            <span className="ofThe">
+                <span className="of">Of</span>
+                <span className="the">The</span>
+            </span>
+            Day
+        </h1>
+        <h3 className="tagline">
+            <span>{props.tagline}</span>
+        </h3>
+    </header>
+)
 
 export default Header;

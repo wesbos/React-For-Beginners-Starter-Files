1e23d592659d4ce363c9fd0bd82b7128b9061547
Garry <gtoborn@gmail.com>
2020-02-11 17:12:37 -0700

    Added setState functionality and AddFishForm

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,27 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    //Old way
+    // constructor() {
+    //     super();
+    // }
+
+    state = {
+        fishes: {},
+        order: {}
+    };
+    
+    addFish = fish => {
+        //1. Take copy of existing state
+        const fishes = {...this.state.fishes};
+        //2. Add our new fish to the fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        //3. Set the new fishes object to state
+        this.setState({
+            fishes: fishes
+        })
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,7 +32,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order otag="Order!!!"/>
-                <Inventory itag="inventory!!!" />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }

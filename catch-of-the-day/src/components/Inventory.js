f344bfefb33c1a8ac86d3f5628efb44a44deca86
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-02-18 19:00:06 -0600

    followed to video 8

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Inventory extends React.Component {
+  render() {
+    return (
+      <div className="inventory">
+        Inventory!!!
+      </div>
+    );
+  }
+}
+
+export default Inventory;

e9654a2b3e2d6949a05a52075aa70bb1753ca9e2
goborn <gtoborn@gmail.com>
2020-02-20 20:18:47 -0700

    added sample fishes function to App

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
     //Old way
@@ -25,6 +26,12 @@ class App extends React.Component {
         })
     }
 
+    loadSampleFishes = () => {
+        this.setState({
+            fishes: sampleFishes
+        })
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -32,7 +39,10 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order otag="Order!!!"/>
-                <Inventory addFish={this.addFish} />
+                <Inventory 
+                addFish={this.addFish}
+                loadSampleFishes={this.loadSampleFishes}
+                />
             </div>
         );
     }

dc2bea4f12299c1ef3835cec4dd677ebc516d691
Garry <gtoborn@gmail.com>
2020-02-24 16:59:02 -0700

    Modified loading sample fishes to show details on page

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
     //Old way
@@ -37,6 +38,9 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                    </ul>
                 </div>
                 <Order otag="Order!!!"/>
                 <Inventory 

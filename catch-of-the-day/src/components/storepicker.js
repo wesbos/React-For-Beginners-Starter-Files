a49e5e58c8a86d324a930d08588d193fc65a284b
spinetree <steve@spinetree.org>
2020-05-25 18:20:47 -0700

    lesson 3

---
+++ catch-of-the-day/src/components/storepicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <form className="store-selector">
+                <h2>Select a Sto</h2>
+            </form>)
+    }
+}
+
+export default StorePicker;
+ 
+// react order of ops - you're improting react, making some stuff, and exporting that stuff
+ 

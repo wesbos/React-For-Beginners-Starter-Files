1db68a674e5f18baa37727ae2104fcd40d28f159
spinetree <steve@spinetree.org>
2020-05-30 14:40:40 -0700

    lesson 5

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom'; // we only need this one method from this ... library?
-
 import StorePicker from './components/storepicker'; //needs relative path to where that components file is
+import "./css/style.css"; // webpack is part of rcreate-react app and is handling this
+
 render(<StorePicker />, document.querySelector('#main')); 
 
 // storepicker is the component we'll spit out into the DOM, we make it a self-closing tag because that saves us having to worry if we closed it, but you don't hafta// react output needs to get mounted to a div in the DOM - can be a div inside things if not using react for everything, or can be the whole page hitting a blank root div

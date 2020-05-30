8621ab4d9efbe9977e848ec68d4d88cbf92f242d
spinetree <steve@spinetree.org>
2020-05-30 10:06:19 -0700

    lesson 4 done

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -5,6 +5,3 @@ import StorePicker from './components/storepicker'; //needs relative path to whe
 render(<StorePicker />, document.querySelector('#main')); 
 
 // storepicker is the component we'll spit out into the DOM, we make it a self-closing tag because that saves us having to worry if we closed it, but you don't hafta// react output needs to get mounted to a div in the DOM - can be a div inside things if not using react for everything, or can be the whole page hitting a blank root div
-
-
-

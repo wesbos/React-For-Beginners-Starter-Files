26a12b0db8e4f8e84a42f3bb8741cf21221b7f4f
spinetree <steve@spinetree.org>
2020-05-30 15:12:33 -0700

    pause mid lesson 6

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Inventory />
+        <Order />
+      </div>
+    );
+  }
+}
+
+export default App;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { render } from 'react-dom'; // we only need this one method from this ... library?
-import StorePicker from './components/storepicker'; //needs relative path to where that components file is
+import React from "react";
+import { render } from "react-dom"; // we only need this one method from this ... library?
+import StorePicker from "./components/storepicker"; //needs relative path to where that components file is
+import App from "./components/App";
 import "./css/style.css"; // webpack is part of rcreate-react app and is handling this
 
-render(<StorePicker />, document.querySelector('#main')); 
+render(<App />, document.querySelector("#main"));
 
 // storepicker is the component we'll spit out into the DOM, we make it a self-closing tag because that saves us having to worry if we closed it, but you don't hafta// react output needs to get mounted to a div in the DOM - can be a div inside things if not using react for everything, or can be the whole page hitting a blank root div

326338519ec400e1da34a7c63c1f53a36cf12d18
spinetree <steve@spinetree.org>
2020-06-27 13:55:05 -0700

    form refs working with console log

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = (event) => {
+    event.preventDefault();
+    const fish = {
+      name: this.nameRef.current.value,
+      price: parseInt(this.priceRef.current.value),
+      status: this.statusRef.current.value,
+      desc: this.descRef.current.value,
+      image: this.imageRef.current.value,
+    };
+    console.log(fish);
+  };
+  render() {
+    return (
+      <div className="AddFishForm">
+        <form className="fish-edit" onSubmit={this.createFish}>
+          <input
+            name="name"
+            ref={this.nameRef}
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            name="price"
+            ref={this.priceRef}
+            type="text"
+            placeholder="Price"
+          />
+          <select name="status" ref={this.statusRef}>
+            <option value="available">Available</option>
+            <option value="unavailable">Sold Oot!</option>
+          </select>
+          <textarea
+            name="desc"
+            ref={this.descRef}
+            type="text"
+            placeholder="Desc"
+          ></textarea>
+          <input
+            name="image"
+            ref={this.imageRef}
+            type="text"
+            placeholder="Image"
+          />
+          <button type="submit">add fish</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddFishForm;
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AddFishForm from "./AddFishForm";
 
 class Inventory extends React.Component {
   render() {
     return (
       <div className="inventory">
-        <i>Inventory</i>
+        <h2>Inventory</h2>
+        <AddFishForm />
       </div>
     );
   }

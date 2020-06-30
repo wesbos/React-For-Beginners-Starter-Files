0519551547a2b0540b9964969bfce32273f791f9
spinetree <steve@spinetree.org>
2020-06-30 12:53:53 -0700

    lesson 15 working

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { formatPrice } from "../helpers";
+class Fish extends React.Component {
+  render() {
+    const { image, name, price, desc, status } = this.props.details;
+    // ES6 destructuring ^^ better way to declare a bunch of variables at once.
+
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={image} />
+        <h3 className="fish-name">
+          {name}
+          <span classname="price">{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button>Add to Cart</button>
+      </li>
+    );
+  }
+}
+
+export default Fish;

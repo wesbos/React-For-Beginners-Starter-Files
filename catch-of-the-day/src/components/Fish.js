51fb5e21c9c5d8d16aa864be96f213c4d9f502d2
spinetree <steve@spinetree.org>
2020-07-03 11:14:18 -0700

    buttons doing conditional thing

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { formatPrice } from "../helpers";
+
 class Fish extends React.Component {
   render() {
     const { image, name, price, desc, status } = this.props.details;
     // ES6 destructuring ^^ better way to declare a bunch of variables at once.
+    const isAvailable = status === "available";
 
     return (
       <li className="menu-fish">
@@ -13,7 +15,9 @@ class Fish extends React.Component {
           <span classname="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to Cart</button>
+        <button disabled={!isAvailable}>
+          {isAvailable ? "Add to Order" : "Sold out"}
+        </button>
       </li>
     );
   }

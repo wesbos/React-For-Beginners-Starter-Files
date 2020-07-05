6af56f4e038206cf76f05956b717fb21da456e66
spinetree <steve@spinetree.org>
2020-07-04 17:26:44 -0700

    price totals working, not in localStorage yet

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -75,7 +75,7 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
   render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === "available";
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+    // remember reduce - take in some data and return a total
     return (
-      <div className="Order">
-        <i>Order</i> component
+      <div className="order-wrap">
+        <h2>order</h2>
+        <ul>
+          <li>{orderIds}</li>
+        </ul>
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
       </div>
     );
   }

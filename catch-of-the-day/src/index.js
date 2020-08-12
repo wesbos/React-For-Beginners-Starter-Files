0f45050ec5c4c122c17c621cb4a87ef874b59a9c
Ryan Beiden <rdbeiden@gmail.com>
2020-08-12 13:00:21 -0500

    add needed components / render App to page

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+import './css/style.css';
+
+render(<App />, document.querySelector('#main'));

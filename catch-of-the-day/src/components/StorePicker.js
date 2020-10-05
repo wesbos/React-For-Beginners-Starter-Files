0959e245fa6beecafaf8d3143d51df6b18198842
gobornG <gtoborn@gmail.com>
2020-10-05 16:43:22 -0600

    Created storepicker component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+    render() {
+        return <p>I am the store picker!</p>
+    }
+}
+
+export default StorePicker;

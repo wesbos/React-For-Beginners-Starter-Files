f16acbe0b4147e0dea15270160d5ef2da06bb449
gobornG <gtoborn@gmail.com>
2020-10-05 19:33:48 -0600

    Update StorePicker.js

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,14 @@ import { render } from 'react-dom';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>I am the store picker!</p>
+        return (
+        <form className="store-selector">
+            { /*comment syntax */}
+            <h2>Please Enter A Store</h2>
+            <input type="text" required placeholder="Store Name" />
+            <button type="submit">Visit Store</button>
+        </form>
+        )
     }
 }
 

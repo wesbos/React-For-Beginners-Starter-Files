290c9c98ccec68b184e2a65822d21ebcaedec358
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-11-11 18:15:52 -0600

    setup

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className='store-selector'>
+        <h2>Please Enter A Store</h2>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+
+render(<StorePicker />, document.querySelector('#main'));

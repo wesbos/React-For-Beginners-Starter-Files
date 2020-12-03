b806410d77a342333d227a249fbfa3e984cb2cda
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-12-03 01:06:09 -0600

    calling it a night

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,17 +2,37 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = (fish) => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({
+      fishes,
+    });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes,
+    })
+  }
+
   render() {
     return (
-    <div className='catch-of-the-day'>
-        <div className="menu">
-            <Header tagline="Fresh Seafood Market" />
+      <div className='catch-of-the-day'>
+        <div className='menu'>
+          <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory />
-    </div>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+      </div>
     );
   }
 }
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
     render() {
         return (
         <div className="inventory">
-            inventory
+            <h2>Inventory</h2>
+            <AddFishForm addFish={this.props.addFish}/>
+            <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
         </div>
         );
     }

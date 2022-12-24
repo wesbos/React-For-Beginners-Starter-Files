9626f27968846a69df19ed5ee4dab93c2c4f59ff
Ventura <daniel.w.ventura@vumc.org>
2022-12-23 22:19:40 -0600

    Build first component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render(){
+        return <p>Hello!</p>
+    }
+}
+
+export default StorePicker;
--- stepped-solutions/Finished App/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import Router from "./components/Router";
-import "./css/style.css";
-
-render(<Router />, document.querySelector("#main"));

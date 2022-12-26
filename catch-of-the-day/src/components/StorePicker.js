c8ae048595c920fb61eff634a3166704fe2b320f
Ventura <daniel.w.ventura@vumc.org>
2022-12-26 17:03:26 -0600

    loading in custom css

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+
+//wes bos react jsx emmet blog post --> set this up!
 class StorePicker extends React.Component {
     render(){
-        return <p>Hello!</p>
+        return (
+                <form className='store-selector'>
+                    <h2>Please Enter a Store</h2>
+                    <input type="text" required placeholder='Store Name'></input>
+                    <button type="submit">Visit Store</button>
+                </form>
+        )
     }
 }
 

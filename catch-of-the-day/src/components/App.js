0e550e95d96b397cbc77956f94acd313ac054047
Ventura <daniel.w.ventura@vumc.org>
2023-01-07 18:17:33 -0600

    Adding boilerplate styling and top-level components

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <div className='catch-of-the-day'>
+                    <div className='menu'>
+                        <Header/>
+                    </div>
+                    <Order/>
+                    <Inventory/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
